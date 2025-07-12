7c343aaa094f36cc284b4a4187c2c3fdf3da1973
Anita <anita.stashevskaya@bytepace.com>
2025-07-12 15:29:02 +0600

    Initial project

---
+++ STest/STest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  STest
+//
+//  Created by Anita Stashevskaya on 12.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+}
+
